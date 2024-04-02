4389749a723248a97a325c56de22664a6a31c684
JaceRogers12 <jacerogers12@gmail.com>
2024-04-02 10:31:28 -0600

    finished project

--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,7 +1,9 @@
 // ✨ create your `quotesSlice` in this module
+import {createSlice} from "@reduxjs/toolkit";
 
 let id = 1
 const getNextId = () => id++
+
 const initialState = {
   displayAllQuotes: true,
   highlightedQuote: null,
@@ -26,3 +28,44 @@ const initialState = {
     },
   ],
 }
+
+const slice = createSlice({
+  name: "quotesSlice",
+  initialState: initialState,
+  reducers: {
+    toggleVisibility(state) {
+      state.displayAllQuotes = !state.displayAllQuotes
+    },
+    deleteQuote(state, action) {
+      state.quotes = state.quotes.filter(quote => quote.id != action.payload)
+    },
+    editQuoteAuthenticity(state, action) {
+      state.quotes = state.quotes.map(quote => {
+        return quote.id != action.payload?
+          quote : {...quote, apocryphal: !quote.apocryphal}
+      })
+    },
+    setHighlightedQuote(state, action) {
+      state.highlightedQuote == action.payload?
+      state.highlightedQuote= null
+      : state.highlightedQuote= action.payload
+    },
+    createQuote: {
+      prepare(authorName, quoteText) {
+        const newQuote = {
+          id: getNextId(),
+          authorName: authorName,
+          quoteText: quoteText,
+          apocryphal: false
+        }
+        return {payload: newQuote}
+      },
+      reducer(state, action) {
+        state.quotes.push(action.payload)
+      }
+    }
+  }
+})
+
+export default slice.reducer;
+export const {toggleVisibility, deleteQuote, editQuoteAuthenticity, setHighlightedQuote, createQuote} = slice.actions;
--- frontend/state/store.js
+++ frontend/state/store.js
@@ -1,1 +1,7 @@
 // ✨ create your `store` in this module
+import {configureStore} from "@reduxjs/toolkit";
+import reducer from "./quotesSlice.js";
+
+export const store = configureStore({
+    reducer: {state: reducer}
+})
